07b0050923506fd680e2fe42b2ad603a7f24409d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-01-06 05:09:28 +0100

    Update msg_server_create_game.go

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	rules "github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -11,6 +13,21 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("System Info not found")
+	}
+	newIndex := strconv.FormatInt(int64(systemInfo.NextId), 10)
+
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+
 	_ = ctx
 
 	return &types.MsgCreateGameResponse{}, nil
