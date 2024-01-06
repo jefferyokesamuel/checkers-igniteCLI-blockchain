7f4f85db21ecd0be4aaeef431ace1d8debad9f4d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-01-06 04:30:41 +0100

    Create msg_server_create_game_test.go

---
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -0,0 +1,28 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/keeper"
+	"github.com/alice/checkers/x/checkers/testutil"
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateGame(t *testing.T) {
+    msgServer, context := setupMsgServer(t)
+    createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
+    require.Nil(t, err)
+    require.EqualValues(t, types.MsgCreateGameResponse{
+        GameIndex: "", // TODO: update with a proper value when updated
+    }, *createResponse)
+}
