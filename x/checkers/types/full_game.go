01e9e4260d606d96362001e5665cda11417220d9
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-01-06 04:31:05 +0100

    Update full_game.go

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -9,44 +9,42 @@ import (
 )
 
 func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
-    black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
-    return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
 }
 
 func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
-    red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
-    return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
 }
 
 func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
-    board, errBoard := rules.Parse(storedGame.Board)
-    if errBoard != nil {
-        return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
-    }
-    board.Turn = rules.StringPieces[storedGame.Turn].Player
-    if board.Turn.Color == "" {
-        return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
-    }
-    return board, nil
+	board, errBoard := rules.Parse(storedGame.Board)
+	if errBoard != nil {
+		return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+	}
+	board.Turn = rules.StringPieces[storedGame.Turn].Player
+	if board.Turn.Color == "" {
+		return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
+	}
+	return board, nil
 }
 
 func (storedGame StoredGame) Validate() (err error) {
-    _, err = storedGame.GetBlackAddress()
-    if err != nil {
-        return err
-    }
-    _, err = storedGame.GetRedAddress()
-    if err != nil {
-        return err
-    }
-    _, err = storedGame.ParseGame()
-    return err
+	_, err = storedGame.GetBlackAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.GetRedAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.ParseGame()
+	return err
 }
 
-
-
 var (
-    ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
-    ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
-    ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
 )
